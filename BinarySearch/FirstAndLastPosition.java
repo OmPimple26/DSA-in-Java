c91b8927e61c18a9cae342ff24c108d40e6c2ca1
OmPimple26 <ompimple04@gmail.com>
2026-06-25 23:38:47 +0530

    Solved Leetcode 34 Problem

---
+++ BinarySearch/FirstAndLastPosition.java
@@ -0,0 +1,57 @@
+import java.util.Arrays;
+
+public class FirstAndLastPosition {
+    public static void main(String[] args) {
+        int[] nums = {5,7,7,8,8,10};
+        int target = 7;
+        int[] ans = searchRange(nums,target);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    public static int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+
+        return new int[]{first, last};
+    }
+
+    private static int findFirst(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        int ans = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                end = mid - 1; // search left side
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    private static int findLast(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        int ans = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                start = mid + 1; // search right side
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
