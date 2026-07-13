8fdd630e9675e17066b153a537b17e692e61d20f
OmPimple26 <ompimple04@gmail.com>
2026-07-13 16:43:03 +0530

    Pretty Printing

---
+++ Strings/PrettyPrinting.java
@@ -0,0 +1,15 @@
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 453.1274f;
+        System.out.printf("Formatted string is %.2f", a);
+
+        System.out.println();
+
+//        System.out.println(Math.PI);
+        System.out.printf("Pie: %.3f",Math.PI);
+
+        System.out.println();
+
+        System.out.printf("Hello my name is %s and I am pretty %s","Om","cool");
+    }
+}
