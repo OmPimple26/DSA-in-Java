65c215926344b8471754379f4d870a9e5b20386c
OmPimple26 <ompimple04@gmail.com>
2026-07-14 20:50:43 +0530

    Palindrome String Problem

---
+++ Strings/StringPalindrome.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class StringPalindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string: ");
+        String str = sc.nextLine();
+        System.out.println(isPalindrome(str));
+    }
+
+    public static boolean isPalindrome(String str){
+        if(str==null || str.length()==0){
+            return true;
+        }
+
+        str = str.toLowerCase();
+
+        for(int i=0; i<=str.length()/2; i++){
+            char start = str.charAt(i);
+            char end = str.charAt(str.length()-1-i);
+
+            if(start!=end){
+                return false;
+            }
+        }
+        return true;
+    }
+}
