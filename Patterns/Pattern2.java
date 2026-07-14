09c7fc2e041cb429befeedbff7f9723deded4692
OmPimple26 <ompimple04@gmail.com>
2026-07-14 22:52:26 +0530

    Code refactored

--- Patterns/Pattern2.java
+++ Patterns/Pattern2.java
@@ -10,10 +10,10 @@
 public class Pattern2 {
     public static void main(String[] args) {
         int n=5;
-        pattern1(n);
+        pattern2(n);
     }
 
-    public static void pattern1(int n){
+    public static void pattern2(int n){
         for(int row=1; row<=n; row++){
             for(int col=1; col<=row; col++){
                 System.out.print("* ");
