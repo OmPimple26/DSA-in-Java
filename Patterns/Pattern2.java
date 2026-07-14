ec56e87da1356acccd32c9042b2d1336822a66db
OmPimple26 <ompimple04@gmail.com>
2026-07-14 22:51:25 +0530

    Pattern 2

---
+++ Patterns/Pattern2.java
@@ -0,0 +1,24 @@
+// Pattern 2:
+
+//*
+//* *
+//* * *
+//* * * *
+//* * * * *
+
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        int n=5;
+        pattern1(n);
+    }
+
+    public static void pattern1(int n){
+        for(int row=1; row<=n; row++){
+            for(int col=1; col<=row; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
