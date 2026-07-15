c868536741cb75d934d59f3f2f55e627e6187cd1
OmPimple26 <ompimple04@gmail.com>
2026-07-15 18:27:28 +0530

    Pattern 1

---
+++ Patterns/Pattern1.java
@@ -0,0 +1,24 @@
+// Pattern 1:
+
+//* * * * *
+//* * * * *
+//* * * * *
+//* * * * *
+//* * * * *
+
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        int n = 5;
+        pattern1(n);
+    }
+
+    public static void pattern1(int n){
+        for(int row=1; row<=n; row++){
+            for(int col=1; col<=n; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
