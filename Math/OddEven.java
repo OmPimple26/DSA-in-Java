4c12fcae96ead9c4e598d593fc8966551232d934
OmPimple26 <ompimple04@gmail.com>
2026-07-26 19:55:50 +0530

    Even or Odd number

---
+++ Math/OddEven.java
@@ -0,0 +1,17 @@
+// Note: Last bit of binary number is also known as less significant bit
+
+// Question - Check whether number is even or odd
+
+public class OddEven {
+    public static void main(String[] args) {
+        System.out.println(isOdd(50));
+    }
+
+    public static boolean isOdd(int n){
+        if((n & 1) == 1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
