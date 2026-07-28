496018069c1d689240ff300181bb6ea9c9aad25f
OmPimple26 <ompimple04@gmail.com>
2026-07-28 22:20:00 +0530

    Power of base

---
+++ Math/bitwise/Power.java
@@ -0,0 +1,34 @@
+//package bitwise;
+//
+//public class Power {
+//    public static void main(String[] args) {
+//        int base = 2;
+//        int power = 5;
+//
+//        int ans = (int)(Math.pow(base,power));
+//        System.out.println(ans);
+//    }
+//}
+
+
+package bitwise;
+
+public class Power {
+    public static void main(String[] args) {
+        int base = 3;
+        int power = 6;
+
+        int ans = 1;
+
+        while(power>0){
+            if((power & 1) == 1){
+                ans = ans * base;
+            }
+
+            base = base*base;
+            power = power>>1;
+        }
+
+        System.out.println(ans);
+    }
+}
