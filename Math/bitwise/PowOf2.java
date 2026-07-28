34e5c1ef106c001f98a509613dffd358911b67be
OmPimple26 <ompimple04@gmail.com>
2026-07-28 18:17:59 +0530

    Power of 2

---
+++ Math/bitwise/PowOf2.java
@@ -0,0 +1,14 @@
+package bitwise;
+
+public class PowOf2 {
+    public static void main(String[] args) {
+        int n = 32;
+
+        if(n==0){
+            System.out.println(false);
+        }else{
+            boolean ans = (n & (n-1)) == 0;
+            System.out.println(ans);
+        }
+    }
+}
