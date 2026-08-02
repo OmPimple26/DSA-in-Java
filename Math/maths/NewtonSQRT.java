9a2f25027720f1d9e7cb4f98e5aac742ce1b268a
OmPimple26 <ompimple04@gmail.com>
2026-08-02 23:24:36 +0530

    Square Root using Newton Method

---
+++ Math/maths/NewtonSQRT.java
@@ -0,0 +1,25 @@
+package maths;
+
+public class NewtonSQRT {
+    public static void main(String[] args) {
+        double n = 40;
+        double ans = sqrt(n);
+        System.out.println(ans);
+    }
+
+    public static double sqrt(double n){
+        double x = n;
+        double root;
+
+        while(true){
+            root = 0.5 * (x + (n/x));
+
+            if(Math.abs(root - x) < 0.5){
+                break;
+            }
+
+            x = root;
+        }
+        return root;
+    }
+}
