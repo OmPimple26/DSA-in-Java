35815d002a8e262b4385369cd77ff0a01573fb36
OmPimple26 <ompimple04@gmail.com>
2026-08-02 18:21:36 +0530

    SQRT using Binary Search

---
+++ Math/maths/SQRTusingBS.java
@@ -0,0 +1,42 @@
+package maths;
+
+public class SQRTusingBS {
+    public static void main(String[] args) {
+        int n = 40;
+        int p = 3;
+        double ans = sqrt(n,p);
+        System.out.println(ans);
+    }
+
+    public static double sqrt(int n, int p){
+        int start = 0;
+        int end = n;
+
+        double root = 0.0;
+
+        while(start<=end){
+            int mid = start + (end-start)/2;
+
+            if(mid * mid == n){
+                return mid;
+            }
+
+            if(mid*mid > n){
+                end = mid-1;
+            }else{
+                start = mid+1;
+                root = mid;
+            }
+        }
+
+        double increment = 0.1;
+        for(int i=0; i<p; i++){
+            while(root * root <= n){
+                root+=increment;
+            }
+            root-=increment;
+            increment/=10;
+        }
+        return root;
+    }
+}
