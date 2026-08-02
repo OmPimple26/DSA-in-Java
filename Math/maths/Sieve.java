ebb0c18cf6bef806c9b1f734c4ccfddff256e084
OmPimple26 <ompimple04@gmail.com>
2026-08-02 15:10:19 +0530

    Sieve method

---
+++ Math/maths/Sieve.java
@@ -0,0 +1,34 @@
+package maths;
+
+public class Sieve {
+    public static void main(String[] args) {
+        int n = 40;
+        boolean[] isPrime = new boolean[n + 1];
+
+        sieve(n, isPrime);
+
+        System.out.println("Prime Numbers:");
+        for (int i = 2; i <= n; i++) {
+            if (isPrime[i]) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    static void sieve(int n, boolean[] isPrime) {
+
+        // Assume all numbers are prime
+        for (int i = 2; i <= n; i++) {
+            isPrime[i] = true;
+        }
+
+        // Mark non-prime numbers
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+    }
+}
