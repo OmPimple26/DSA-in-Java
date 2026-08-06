0d82b5d9e45f5ef180068997649743173bc2ed98
OmPimple26 <ompimple04@gmail.com>
2026-08-06 22:38:14 +0530

    Print n to 1 and then 1 to n

--- Recursion/easy/Nto1.java
+++ Recursion/easy/Nto1.java
@@ -3,7 +3,9 @@ package easy;
 public class Nto1 {
     public static void main(String[] args) {
 //        fun(5);
-        funRev(5);
+//        funRev(5);
+//        funRevPart2(1);
+        funBoth(5);
     }
 
     // Method 1 - Print n to 1
@@ -22,6 +24,26 @@ public class Nto1 {
             return;
         }
         funRev(n-1);
+        System.out.println(n);
+    }
+
+//    public static void funRevPart2(int n){
+//        if(n==5){
+//            System.out.println(5);
+//            return;
+//        }
+//        System.out.println(n);
+//        funRevPart2(n+1);
+//    }
+
+    // Method 3 - Print n to 1 and then 1 to n
+    public static void funBoth(int n){
+        if(n==0){
+            return;
+        }
+
+        System.out.println(n);
+        funBoth(n-1);
         System.out.println(n);
     }
 }
