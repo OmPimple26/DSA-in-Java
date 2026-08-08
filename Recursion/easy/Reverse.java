ac4fe2870be514dd35d5b9c4cd6030a7bd63973c
OmPimple26 <ompimple04@gmail.com>
2026-08-08 22:45:54 +0530

    Reverse using Recursion

---
+++ Recursion/easy/Reverse.java
@@ -0,0 +1,20 @@
+package easy;
+
+public class Reverse {
+    public static int sum = 0;
+
+    public static void rev1(int n){
+        if(n==0){
+            return;
+        }
+
+        int rem = n%10;
+        sum = sum*10 + rem;
+        rev1(n/10);
+    }
+
+    public static void main(String[] args) {
+        rev1(1234);
+        System.out.println(sum);
+    }
+}
