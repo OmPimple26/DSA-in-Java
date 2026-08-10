9109384d509bd152256089b9bbad395aeda84c6d
OmPimple26 <ompimple04@gmail.com>
2026-08-10 20:41:36 +0530

    Find multiple indexes of an element

--- Recursion/arrays/Find.java
+++ Recursion/arrays/Find.java
@@ -1,11 +1,20 @@
 package arrays;
 
+import java.util.ArrayList;
+
 public class Find {
     public static void main(String[] args) {
         int[] arr = {2, 3, 1, 4, 4, 5};
-        System.out.println(find(arr, 4, 0));
-        System.out.println(findIndex(arr, 4, 0));
-        System.out.println(findIndexLast(arr, 4, arr.length-1));
+//        System.out.println(find(arr, 4, 0));
+//        System.out.println(findIndex(arr, 4, 0));
+//        System.out.println(findIndexLast(arr, 4, arr.length-1));
+//        findAllIndex(arr, 4 ,0);
+//        System.out.println(list);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> ans = findAllIndex1(arr, 4, 0, list);
+        System.out.println(ans);
+        System.out.println(list);
     }
 
     public static boolean find(int[] arr, int target, int index){
@@ -35,5 +44,26 @@ public class Find {
         }else{
             return findIndex(arr, target, index-1);
         }
+    }
+
+    public static ArrayList<Integer> list = new ArrayList<>();
+    public static void findAllIndex(int[] arr, int target, int index){
+        if(index==arr.length){
+            return;
+        }
+        if(arr[index]==target){
+            list.add(index);
+        }
+        findAllIndex(arr, target, index+1);
+    }
+
+    public static ArrayList<Integer> findAllIndex1(int[] arr, int target, int index, ArrayList<Integer> list){
+        if(index==arr.length){
+            return list;
+        }
+        if(arr[index]==target){
+            list.add(index);
+        }
+        return findAllIndex1(arr, target, index+1, list);
     }
 }
