d128608e0ff1d6f21ccdbcd4abeb7866644d5b7d
OmPimple26 <ompimple04@gmail.com>
2026-08-14 23:16:16 +0530

    Bubble Sort using Recursion

--- Recursion/patterns/Triangle.java
+++ Recursion/patterns/Triangle.java
@@ -1,9 +1,15 @@
 package patterns;
 
+import java.util.Arrays;
+
 public class Triangle {
     public static void main(String[] args) {
 //        triangle(4,0);
-        triangle2(4,0);
+//        triangle2(4,0);
+
+        int arr[] = {1,4,3,5};
+        bubble(arr,arr.length-1,0);
+        System.out.println(Arrays.toString(arr));
     }
 
     public static void triangle(int r, int c){
@@ -31,6 +37,25 @@ public class Triangle {
         }else{
             triangle2(r-1, 0);
             System.out.println();
+        }
+    }
+
+    public static void bubble(int[] arr, int r, int c){
+        if(r==0){
+            return;
+        }
+
+        if(c<r){
+            if(arr[c]>arr[c+1]){
+                //swap
+                int temp = arr[c];
+                arr[c] = arr[c+1];
+                arr[c+1] = temp;
+            }
+
+            bubble(arr, r, c+1);
+        }else{
+            bubble(arr, r-1, 0);
         }
     }
 }
