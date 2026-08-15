b0e9803cfe4224691b4c582c7ba0647295b4b824
OmPimple26 <ompimple04@gmail.com>
2026-08-15 23:43:09 +0530

    Selection Sort - Triangle

--- Recursion/patterns/Triangle.java
+++ Recursion/patterns/Triangle.java
@@ -7,8 +7,12 @@ public class Triangle {
 //        triangle(4,0);
 //        triangle2(4,0);
 
+//        int arr[] = {1,4,3,5};
+//        bubble(arr,arr.length-1,0);
+//        System.out.println(Arrays.toString(arr));
+
         int arr[] = {1,4,3,5};
-        bubble(arr,arr.length-1,0);
+        selection(arr, arr.length, 0, 0);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -56,6 +60,24 @@ public class Triangle {
             bubble(arr, r, c+1);
         }else{
             bubble(arr, r-1, 0);
+        }
+    }
+
+    public static void selection(int[] arr, int r, int c, int max) {
+        if (r == 0) {
+            return;
+        }
+        if (c < r) {
+            if (arr[c] > arr[max]) {
+                selection(arr, r, c+1, c);
+            } else {
+                selection(arr, r, c+1, max);
+            }
+        } else {
+            int temp = arr[max];
+            arr[max] = arr[r-1];
+            arr[r-1] = temp;
+            selection(arr, r-1, 0, 0);
         }
     }
 }
