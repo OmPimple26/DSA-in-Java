b52d2739aa56e4be6c22004bd0553a832c06def2
OmPimple26 <ompimple04@gmail.com>
2026-08-20 23:21:51 +0530

    Initial Commit

---
+++ OOPS/introduction/Main.java
@@ -0,0 +1,71 @@
+// Class is a named group of properties and functions
+// If we have to create our own datatype, then we create it using classes
+
+// Class is a blueprint for creating an object / Class is a template of an object
+// Object is an instance of a class
+
+// Class = Logical Construct
+// Object = Physical Reality (Occupies space in memory)
+
+// Properties of an object-
+// i) State - Value from its datatype
+// ii) Identity - Each object is different from other object
+// iii) Behaviour - Effect of datatype operations
+
+// Reference variables are stored in stack memory and objects are stored in heap memory and these reference variables present in the stack memory points to the objects in the heap memory
+
+// Variables inside the object are called instance variables -> Declared outside the method but inside the class
+// Variables that define an object are called reference variables
+// e.g. In student1.age, student1 is reference variable while age is an instance variable
+
+// new keyword = Dynamic Memory Allocation
+
+// For e.g.
+// Student student1 = new Student();
+// Student student1 -> Occurs at Compile Time
+// new Student() -> Occurs at Run Time
+
+
+package introduction;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args){
+        // Store 5 roll nos
+        int[] numbers = new int[5];
+
+        // Store 5 names
+        String[] names = new String[5];
+
+        // Data of 5 students: {roll no, name, marks}
+        int[] rno = new int[5];
+        String[] name = new String[5];
+        float[] marks = new float[5];
+
+        Student[] students = new Student[5];
+
+          // just declaring
+//        Student kunal;
+//        System.out.println(Arrays.toString(students));
+//        kunal = new Student();
+
+        Student om = new Student();
+
+        om.rno = 33;
+        om.name = "Om Pimple";
+        om.marks = 72.70f;
+
+        System.out.println(om.rno);
+        System.out.println(om.name);
+        System.out.println(om.marks);
+    }
+
+    // Create a class for every single student
+    static class Student{
+        int rno;
+        String name;
+        float marks;
+//        float marks = 80;
+    }
+}
