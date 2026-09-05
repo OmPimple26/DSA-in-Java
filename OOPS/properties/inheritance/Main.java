74e6e2ea8fed860c066cdb6671a9db0024878c36
OmPimple26 <ompimple04@gmail.com>
2026-09-05 23:27:11 +0530

    Initial Commit

--- OOPS/properties/inheritance/Main.java
+++ OOPS/properties/inheritance/Main.java
@@ -4,16 +4,19 @@ package properties.inheritance;
 
 public class Main {
     public static void main(String[] args) {
-        Box box = new Box();
-        System.out.println(box.l + " " + box.w + " " + box.h);
+//        Box box = new Box();
+//        System.out.println(box.l + " " + box.w + " " + box.h);
+//
+//        Box box1 = new Box(4);
+//        System.out.println(box1.l + " " + box1.w + " " + box1.h);
+//
+//        Box box2 = new Box(4.6, 7.9, 9.9);
+//        System.out.println(box2.l + " " + box2.w + " " + box2.h);
+//
+//        Box box3 = new Box(box2);
+//        System.out.println(box3.l + " " + box3.w + " " + box3.h);
 
-        Box box1 = new Box(4);
-        System.out.println(box1.l + " " + box1.w + " " + box1.h);
-
-        Box box2 = new Box(4.6, 7.9, 9.9);
-        System.out.println(box2.l + " " + box2.w + " " + box2.h);
-
-        Box box3 = new Box(box2);
-        System.out.println(box2.l + " " + box2.w + " " + box2.h);
+        BoxWeight box4 = new BoxWeight();
+        System.out.println(box4.l + " " + box4.w + " " + box4.h + " " + box4.weight);
     }
 }
