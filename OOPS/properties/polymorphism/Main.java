5da390bdd827959d60084c5c5bf3ef3ed677ae86
OmPimple26 <ompimple04@gmail.com>
2026-09-14 23:11:25 +0530

    Initial Commit

--- OOPS/properties/polymorphism/Main.java
+++ OOPS/properties/polymorphism/Main.java
@@ -4,6 +4,17 @@
 
 // It is an act of representing the same thing in multiple ways
 
+// Types of Polymorphism:
+// 1) Compile Time / Static Polymorphism ->
+// Achieved via method overloading
+
+// Method overloading -> When a class has multiple methods with the same name but the number and types, arguments, return types, ordering can be different
+// For e.g. Multiple Constructors
+// A a = new A();
+// A a2 = new A(3,4);
+
+// Java decides which constructor or method to call at compile time hence it is knoen as compile time polymorphism
+
 package properties.polymorphism;
 
 public class Main {
